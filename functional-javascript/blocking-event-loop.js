792f4345d098dac76a353033c4e720958f37769e
Bruno Sanches <brunopsanches@gmail.com>
2014-01-04 20:12:58 -0800

    by releasing control after 400 recursive calls, 801 operations were reached (approximately 270 before that)

--- functional-javascript/blocking-event-loop.js
+++ functional-javascript/blocking-event-loop.js
@@ -1,10 +1,17 @@
 function repeat(operation, num) {
   // modify this so it can be interrupted
   if (num <= 0) return;
-  setTimeout(function() {
-    operation()
+
+  operation();
+
+  if (num % 400 === 0) {
+    setTimeout(function() {
+      repeat(operation, --num);
+    }, 0);
+  }
+  else {
     repeat(operation, --num);
-  }, 0);
+  }
 }
 
 module.exports = repeat
