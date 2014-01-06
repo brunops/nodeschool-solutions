286124393d74f44a832e163fcbe33437d6ee7fe8
Bruno Sanches <brunopsanches@gmail.com>
2014-01-06 00:07:37 -0800

    Asynchronously load all users, run done callback when everything finishes
    * Solved loadUsers asynchronously using recursion because the problem
    stated not to use while/for loops
    * Program sometimes times out
    * Official solving version uses .forEach - so I'll rewrite it with it
    too 'cause recursive version is slow

--- functional-javascript/async-loops.js
+++ functional-javascript/async-loops.js
@@ -1,9 +1,33 @@
 function loadUsers(userIds, load, done) {
-  var users = []
-  for (var i = 0; i < userIds.length; i++) {
-    users.push(load(userIds[i]))
-  }
-  return users
+  var users = [],
+      userIndex = 0;
+
+  var usersLoaded = 0;
+
+  var loadAsync = function(userIndex) {
+    load(userIds[userIndex], (function(index) {
+      return function(user) {
+        ++usersLoaded;
+        if (user) {
+          users[index] = user;
+        }
+
+        if (usersLoaded === userIds.length) {
+          done(users);
+        }
+      }
+
+    })(userIndex));
+
+
+    userIndex++;
+    if (userIndex < userIds.length) {
+      loadAsync(userIndex);
+    }
+  };
+
+  loadAsync(0);
 }
+
 
 module.exports = loadUsers
