270d489001566ee4b0de016d38c3ef5bde2ec0dc
Bruno Sanches <brunopsanches@gmail.com>
2014-01-05 21:33:55 -0800

    Implement trampoline function, still confusing - really helpful article here http://raganwald.com/2013/03/28/trampolines-in-javascript.html

--- functional-javascript/trampoline.js
+++ functional-javascript/trampoline.js
@@ -1,23 +1,20 @@
 function repeat(operation, num) {
-  // Modify this so it doesn't cause a stack overflow!
-  if (num <= 0) return
-  operation()
-  return repeat(operation, --num)
+  return num
+  ? function() { operation(); return repeat(operation, num - 1); } 
+  : 0;
 }
 
 function trampoline(fn) {
-  // You probably want to implement a trampoline!
+  var result = fn();
 
-  var result = fn.apply(fn, arguments);
-  while (typeof result === 'function') {
-    result = result();
+  while (result && typeof result === 'function') {
+    result = result.apply(result, arguments);
   }
 
   return result;
 }
 
 module.exports = function(operation, num) {
-  // You probably want to call your trampoline here!
-  return repeat(operation, num)
+  return trampoline(repeat(operation, num));
 }
 
