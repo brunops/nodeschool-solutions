ce07e152c7e6965a1fe0b329c1b889602ab8e591
Bruno Sanches <brunopsanches@gmail.com>
2014-01-22 20:07:33 -0800

    Exercise 2 - Jade templating rendering

---
+++ expressworks/jade.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+    path = require('path');
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', process.argv[3] || path.join(__dirname, 'templates'));
+
+app.get('/home', function(req, res) {
+  res.render('index', { date: new Date().toDateString() });
+});
+app.listen(process.argv[2] || 3000);
