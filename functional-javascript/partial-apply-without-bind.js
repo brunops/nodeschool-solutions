854b938e1b2d673372d69a08dcdc6374fad0dc05
Bruno Sanches <brunopsanches@gmail.com>
2014-02-24 17:12:01 -0800

    Partial apply without #bind

---
+++ functional-javascript/partial-apply-without-bind.js
@@ -0,0 +1,9 @@
+var slice = Array.prototype.slice;
+
+function logger(namespace) {
+  return function() {
+    console.log.apply(console, [namespace].concat(slice.call(arguments)));
+  }
+}
+
+module.exports = logger;
