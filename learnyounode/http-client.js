4d21c76bc7948a0ab96cd5274b87ca68e4cbcc96
Bruno Sanches <brunopsanches@gmail.com>
2014-04-19 22:01:21 -0700

    Learn you node - http client

---
+++ learnyounode/http-client.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function (response) {
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log(data);
+  });
+});
+
