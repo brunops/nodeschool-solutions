5cfe0f032944adc9e5cb027e640171f08db0afc2
Bruno Sanches <brunopsanches@gmail.com>
2014-04-22 00:41:04 -0700

    Stream Adventure - Concat

---
+++ stream-adventure/concat.js
@@ -0,0 +1,7 @@
+var concatStream = require('concat-stream');
+
+process.stdin
+  .pipe(concatStream(function (body) {
+    process.stdout.write(body.toString().split('').reverse().join('') + "\n");
+  }));
+
