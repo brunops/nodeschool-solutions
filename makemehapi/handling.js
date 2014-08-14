99d2c2d84dd43285fbdb8544b1c4ba2684ef76ca
Bruno Sanches <brunopsanches@gmail.com>
2014-08-13 19:19:20 -0700

    Make me Hapi - Exercise 3

---
+++ makemehapi/handling.js
@@ -0,0 +1,15 @@
+var Hapi = require('hapi');
+var path = require('path');
+
+var server = Hapi.createServer('localhost', process.argv[2] || 8080);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: {
+    file: path.join(__dirname, 'index.html')
+  }
+});
+
+server.start();
+
