ca3fd7de545da9f28b28669d06631b6e27c397fb
Bruno Sanches <brunopsanches@gmail.com>
2015-06-10 18:36:10 -0700

    Learn you React - Hello React

---
+++ learnyoureact/hello-react/program.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.argv[2] || 3000);
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+require('node-jsx').install();
+
+app.use('/', function (req, res) {
+  res.render('index', '');
+});
+
+app.listen(app.get('port'), function () {
+  console.log('server listening on port %s', app.get('port'));
+});
---
+++ learnyoureact/hello-react/views/index.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var TodoBox = React.createClass({
+  render: function () {
+    return (
+      <div className="todoBox">
+        Hello, world!
+      </div>
+    );
+  }
+});
+
+module.exports = TodoBox;
