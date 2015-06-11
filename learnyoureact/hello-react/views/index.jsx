6ef96ea3b81d09dfc483154a3d99cac2b6e78876
Bruno Sanches <brunopsanches@gmail.com>
2015-06-10 18:44:56 -0700

    Learn you React - Components

--- learnyoureact/hello-react/views/index.jsx
+++ learnyoureact/hello-react/views/index.jsx
@@ -4,7 +4,29 @@ var TodoBox = React.createClass({
   render: function () {
     return (
       <div className="todoBox">
-        Hello, world!
+        <h1>Todos</h1>
+        <TodoList />
+        <TodoForm />
+      </div>
+    );
+  }
+});
+
+var TodoList = React.createClass({
+  render: function () {
+    return (
+      <div className="todoList">
+        I am a TodoList.
+      </div> 
+    );
+  }
+});
+
+var TodoForm = React.createClass({
+  render: function () {
+    return (
+      <div className="todoForm">
+        I am a TodoForm.
       </div>
     );
   }
