cce7388761da81165070daf236b7304d93c82b45
Bruno Sanches <brunopsanches@gmail.com>
2015-06-10 21:02:47 -0700

    Learn You React - CSS

--- learnyoureact/hello-react/views/index.jsx
+++ learnyoureact/hello-react/views/index.jsx
@@ -45,9 +45,9 @@ var Todo = React.createClass({
   render: function () {
     return (
       <tr>
-        <td style={{border: "1px solid black"}}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
-        <td style={{border: "1px solid black"}}>{this.props.title}</td>
-        <td style={{border: "1px solid black"}}>{this.props.children}</td>
+        <td style={style.tableContent}><input type="checkbox" checked={this.state.checked} onChange={this.handleChange} /></td>
+        <td style={style.tableContent}>{this.props.title}</td>
+        <td style={style.tableContent}>{this.props.children}</td>
       </tr>
     );
   }
@@ -62,5 +62,11 @@ var TodoForm = React.createClass({
     );
   }
 });
+
+var style = {
+  tableContent: {
+    border: '1px solid black'
+  }
+};
 
 module.exports = TodoBox;
