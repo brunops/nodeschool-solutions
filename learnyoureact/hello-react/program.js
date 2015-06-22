d751e3aee41557ec387ff47aa22a618ba68196fd
Bruno Sanches <brunopsanches@gmail.com>
2015-06-21 20:35:24 -0700

    Learn You React - Isomorphic

--- learnyoureact/hello-react/program.js
+++ learnyoureact/hello-react/program.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 
+var React = require('react');
+var DOM = React.DOM;
+var body = DOM.body;
+var div = DOM.div;
+var script = DOM.script;
+
+var browserify = require('browserify');
+
 app.set('port', process.argv[2] || 3000);
 app.set('view engine', 'jsx');
 app.set('views', __dirname + '/views');
 app.engine('jsx', require('express-react-views').createEngine());
 
 require('node-jsx').install();
+var TodoBox = require('./views/index.jsx');
 
 var data = [
   {
@@ -19,10 +28,40 @@ var data = [
   }
 ];
 
+app.use('/bundle.js', function (req, res) {
+  res.setHeader('Content-Type', 'application/javascript');
+  browserify('./app.js')
+    .transform('reactify')
+    .bundle()
+    .pipe(res);
+});
+
 app.use('/', function (req, res) {
-  res.render('index', { data: data });
+  var initialData = JSON.stringify(data);
+  var markup = React.renderToString(React.createElement(TodoBox, {data: data}));
+
+  res.setHeader('Content-Type', 'text/html');
+
+  var html = React.renderToStaticMarkup(body(null,
+      div({ id: 'app', dangerouslySetInnerHTML: { __html: markup }}),
+      script({ id: 'initial-data',
+        type: 'text/plain',
+        'data-json': initialData
+      }),
+      script({ src: '/bundle.js' })
+  ));
+
+  res.end(html);
 });
 
 app.listen(app.get('port'), function () {
   console.log('server listening on port %s', app.get('port'));
 });
+
+
+
+
+
+
+
+
