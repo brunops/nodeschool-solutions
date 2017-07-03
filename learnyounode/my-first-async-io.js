0df3aff5ba07d3ca373205f8c28378e74d90796c
Edwin Moses <workockmoses@gmail.com>
2017-07-03 16:43:10 +0300

    added es6 const and arrow function

--- learnyounode/my-first-async-io.js
+++ learnyounode/my-first-async-io.js
@@ -1,6 +1,6 @@
-var fs = require('fs');
+const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
   if (err) throw err;
   console.log(data.split('\n').length - 1);
 });
