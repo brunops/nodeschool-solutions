5ba7a8aab1b09e883b83121fb6e7562ff020734e
Edwin Moses <workockmoses@gmail.com>
2017-07-03 16:29:06 +0300

    es6 synthax

--- learnyounode/make-it-modular.js
+++ learnyounode/make-it-modular.js
@@ -3,13 +3,13 @@ var filterDir = require('./filter-dir');
 var dirPath = process.argv[2],
     extension = process.argv[3];
 
-filterDir(dirPath, extension, function (err, list) {
+filterDir(dirPath, extension, (err, list) => {
   if (err) {
     console.log('An error happened when reading ' + dirPath);
     return err;
   }
   
-  list.forEach(function (filename) {
+  list.forEach((filename) => {
     console.log(filename);
   });
 });
