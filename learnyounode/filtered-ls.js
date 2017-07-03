f08c3255c9be0649f3e66bf887a3d8e28c237344
Edwin Moses <workockmoses@gmail.com>
2017-07-03 16:36:30 +0300

    added es6 and arrow function synthax

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readdir(process.argv[2], function (err, list) {
-  list.forEach(function (filename) {
+fs.readdir(process.argv[2], (err, list) => {
+  list.forEach((filename) => {
     if (path.extname(filename) === '.' + process.argv[3]) {
       console.log(filename);
     }
