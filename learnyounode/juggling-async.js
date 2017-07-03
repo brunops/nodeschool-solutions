1ea20754e2b4c8bb12c854c28e9b72c1008024ca
Edwin Moses <workockmoses@gmail.com>
2017-07-04 00:58:53 +0300

    added es6 arrow function

--- learnyounode/juggling-async.js
+++ learnyounode/juggling-async.js
@@ -5,16 +5,16 @@ var urls = process.argv.slice(2),
     results = [],
     resultsCount = 0;
 
-urls.forEach(function (url, i) {
-  http.get(url, function (response) {
+urls.forEach((url, i) => {
+  http.get(url, (response) => {
     response.setEncoding('utf8');
 
-    response.pipe(concatStream(function (data) {
+    response.pipe(concatStream((data) => {
       results[i] = data;
       resultsCount++;
 
       if (resultsCount === urls.length) {
-        results.forEach(function (result) {
+        results.forEach((result) => {
           console.log(result);
         });
       }
