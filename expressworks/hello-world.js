81eb9cf61b034f10749224a0a6ded9203e752a92
Edwin Moses <workockmoses@gmail.com>
2017-07-04 12:54:47 +0300

    added es6 arrow function

--- expressworks/hello-world.js
+++ expressworks/hello-world.js
@@ -1,7 +1,7 @@
 var express = require('express');
 
 var app = express();
-app.get('/home', function(req, res) {
+app.get('/home', (req, res) => {
   res.send('Hello World!');
 });
 app.listen(process.argv[2] || 3000);
